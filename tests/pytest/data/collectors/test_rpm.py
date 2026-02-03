311f1e600e1e5aff4d7dea769202b49c716b6e3b
Souta Kawahara <souta.kawahara@miraclelinux.com>
2026-02-03 21:18:16 +0900

    feat: pytest: Implement test feature for EPEL9 package (as a 3rd party package)
    Signed-off-by: Souta Kawahara <souta.kawahara@miraclelinux.com>

--- tests/pytest/data/collectors/test_rpm.py
+++ tests/pytest/data/collectors/test_rpm.py
@@ -38,6 +38,31 @@ EXPECTED_ALMA_PACKAGE = Package(
     alma_pkg=True,
 )
 
+TESTED_EPEL_PACKAGE_NAME = 'dummy-package-canary-2-1.el9.noarch.rpm'
+TESTED_EPEL_PACKAGE_PATH = os.path.dirname(__file__) + f'/{TESTED_EPEL_PACKAGE_NAME}'
+
+# EXPECTED_EPEL_PACKAGE_LICENSES = licenses=Licenses(ids=['CC-PDDC'], expression='CC-PDDC')
+EXPECTED_EPEL_PACKAGE_HASH_VALUE = '0400684742829c5f94f75a4ed86feb0d2e21e2dce27be60451643df02a9323d5'
+EXPECTED_EPEL_PACKAGE = Package(
+    package_nevra=PackageNevra( # dummy-package-canary-2-1.el9.noarch
+        epoch = None,
+        name = 'dummy-package-canary',
+        version = '2',
+        release = '1.el9',
+        arch = 'noarch',
+    ),
+    source_rpm='dummy-package-canary-2-1.el9.src.rpm',
+    # package_timestamp=None,
+    hashs=[Hash(
+        value='0400684742829c5f94f75a4ed86feb0d2e21e2dce27be60451643df02a9323d5',
+        algorithm=Algorithms.SHA_256,
+    )],
+    licenses=Licenses(ids=['CC-PDDC'], expression='CC-PDDC'),
+    summary='Dummy package to exercise the packaging stack',
+    description='This a dummy canary package to exercise the packaging stack and make sure it\nactually works as expected (e.g. by removing and installing this package as\npart of a configuration management flow).',
+    alma_pkg=False, ### (means this package isn't from AlmaLinux)
+)
+
 @pytest.fixture
 def rpm_collector_instance() -> RpmCollector:
     return RpmCollector()
@@ -46,8 +71,10 @@ def rpm_collector_instance() -> RpmCollector:
 def test_collect_package_from_file(rpm_collector_instance: RpmCollector) -> None:
     # TODO: use path
     assert rpm_collector_instance.collect_package_from_file(TESTED_ALMA_PACKAGE_PATH) == EXPECTED_ALMA_PACKAGE
+    assert rpm_collector_instance.collect_package_from_file(TESTED_EPEL_PACKAGE_PATH) == EXPECTED_EPEL_PACKAGE
 
 
 def test_hash_file() -> None:
     assert hash_file(TESTED_ALMA_PACKAGE_PATH) == EXPECTED_ALMA_PACKAGE_HASH_VALUE
+    assert hash_file(TESTED_EPEL_PACKAGE_PATH) == EXPECTED_EPEL_PACKAGE_HASH_VALUE
 
